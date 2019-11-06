91e5665362220157338cf6a617bee04e35cc6b89
MaganaLu <49489590+MaganaLu@users.noreply.github.com>
2019-11-05 23:53:23 -0800

    Version 3
    Working parentheses method 
    
    To Do:
    finish error handling on wrong inputs
    fix error on too many parentheses

--- Java_Calculator/src/java_calculator/Java_Calculator.java
+++ Java_Calculator/src/java_calculator/Java_Calculator.java
@@ -6,8 +6,6 @@
 package java_calculator;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -21,104 +19,131 @@ public class Java_Calculator {
      */
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-   
-        
-        System.out.println("Enter the calculation: ");
+
+        System.out.println("Enter the calculation with spaces after each character: ");
         String userCalc = in.nextLine();
-        
+
         String[] splitArray = userCalc.split(" ");
-        
+        //correctInCheck(splitArray);
+
         ArrayList<String> userCalculationList = new ArrayList<>();
-        for(int i = 0; i < splitArray.length; i++){
-            userCalculationList.add(splitArray[i]);
+        for (String splitArray1 : splitArray) {
+            userCalculationList.add(splitArray1);
         }
-        
-        
+
         solve(userCalculationList);
-        
+
         System.out.println(userCalculationList);
-        
+
         System.out.println(userCalculationList.get(0));
-        
-        
-        
-        
-     
     }
-    
+
     // checks to see if input is an operand
-    public boolean isOp(String ch){
+    public boolean isOp(String ch) {
+
+        return ch.contains("+") || ch.contains("-") || ch.contains("*")
+                || ch.contains("/") || ch.contains("^") || ch.contains("(")
+                || ch.contains(")");
+    }
+
+    public static int solve(ArrayList<String> userCalculationList) {
         
-        if(ch.contains("+") || ch.contains("-") || ch.contains("*") || 
-                ch.contains("/") || ch.contains("^") || ch.contains("(") || 
-                ch.contains(")")){
-            return true;
-        }
-        
-        return false;
-}
-    
-    public static int solve(ArrayList<String> userCalculationList){
-        //expression = calculate();
-        // calculate = computation
+        parentheses(userCalculationList);
         
         computation(userCalculationList, "^");
         computation(userCalculationList, "+");
         computation(userCalculationList, "-");
         computation(userCalculationList, "*");
         computation(userCalculationList, "/");
-        
-        
+
         return Integer.parseInt(userCalculationList.get(0));
-        
-        
     }
-    
+
+    public static ArrayList<String> parentheses(ArrayList<String> userCalculationList) {
+
+        for (int i = 0; i < userCalculationList.size(); i++) {
+            if (userCalculationList.get(i).equals("(")) {
+                int start = 1;
+                int end = 0;
+
+                for (int x = i + 1; x < userCalculationList.size(); x++) {
+                    if (userCalculationList.get(x).equals("(")) {
+                        start = start + 1;
+                    } else if (userCalculationList.get(x).equals(")")) {
+                        end = end + 1;
+
+                        if (start == end) {
+                            ArrayList<String> parenthesesAList = new ArrayList<>();
+
+                            for (int k = i + 1; k < x; k++) {
+                                parenthesesAList.add(userCalculationList.get(k));
+                            }
+                            // runs the solve method within the new array
+                            userCalculationList.set(i, Integer.toString(solve(parenthesesAList)));
+
+                            for (int m = i + 1; m <= x; m++) {
+                                userCalculationList.remove(i + 1);
+                            }
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        return userCalculationList;
+    }
+
     //perform operation on ArrayLiost and new ArrayList
-    public static ArrayList<String> computation(ArrayList<String> userCalculationList, String operand){
-        for(int i = 0; i < userCalculationList.size(); i++){
-            if(userCalculationList.get(i).equals(operand)){
-                
-                int a = Integer.parseInt(userCalculationList.get(i-1));
-                int b = Integer.parseInt(userCalculationList.get(i+1));
-                
-                userCalculationList.set(i-1, Integer.toString(operationCalc(a,b,operand)));
-                
+    public static ArrayList<String> computation(ArrayList<String> userCalculationList, String operand) {
+        for (int i = 0; i < userCalculationList.size(); i++) {
+
+            // check to see if the element was an operand
+            if (userCalculationList.get(i).equals(operand)) {
+
+                int a = Integer.parseInt(userCalculationList.get(i - 1));
+                int b = Integer.parseInt(userCalculationList.get(i + 1));
+
+                // the set method is used to set an element in ana ArrayList on the specified index
+                userCalculationList.set(i - 1, Integer.toString(operationCalc(a, b, operand)));
+
                 // remove the elemenst in the list except for the first one
                 userCalculationList.remove(i);
                 userCalculationList.remove(i);
-                
+
                 // go back down to the last element if the elemnt was an operand
                 i--;
-                
+
             }
         }
         return userCalculationList;
     }
     
+    /*    public static void correctInCheck(String[] splitArray){
+    for(int i = 0; 1 < splitArray.length; i++){
+    if(){
     
-    // calculates the value of two numbers together
-    public static int operationCalc(int a, int b, String operand){
-        
-        if(operand.equals("^")){
-            return (int)Math.pow(a,b);
-        }
-        else if(operand.equals("+")){
-            return a + b;
-        }
-        else if(operand.equals("-")){
-            return a - b;
-        }
-        else if(operand.equals("*")){
-            return a *b;
-        }
-        else if(operand.equals("/")){
-            return a / b;
-        }
-        else return -1;
+    }
     }
     
-    
+    }*/
+
+    // calculates the value of two numbers together
+    public static int operationCalc(int a, int b, String operand) {
+
+        switch (operand) {
+            case "^":
+                return (int) Math.pow(a, b);
+            case "+":
+                return a + b;
+            case "-":
+                return a - b;
+            case "*":
+                return a * b;
+            case "/":
+                return a / b;
+            default:
+                return -1;
+        }
+    }
 
 }
-
